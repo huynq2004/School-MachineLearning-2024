46dbb26209554659eab4575e9297fdd47183ba8c
huynq2004 <huynguyenfw@gmail.com>
2024-10-08 03:43:25 +0700

    Add support vector machine eg

---
+++ Support Vector Machine/svm.py
@@ -0,0 +1,111 @@
+from __future__ import print_function
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
+from matplotlib.backends.backend_pdf import PdfPages  # <-- Add this line
+np.random.seed(22)
+
+means = [[2, 2], [4, 2]]
+cov = [[.3, .2], [.2, .3]]
+N = 10
+X0 = np.random.multivariate_normal(means[0], cov, N) # class 1
+X1 = np.random.multivariate_normal(means[1], cov, N) # class -1 
+X = np.concatenate((X0.T, X1.T), axis = 1) # all data 
+y = np.concatenate((np.ones((1, N)), -1*np.ones((1, N))), axis = 1) # labels 
+
+
+# plot points
+plt.plot(X0[:, 0], X0[:, 1], 'bs', markersize = 8, alpha = .8)
+plt.plot(X1[:, 0], X1[:, 1], 'ro', markersize = 8, alpha = .8)
+plt.axis('equal')
+# axis limits
+plt.ylim(0, 3)
+plt.xlim(2, 4)
+
+
+# hide tikcs 
+cur_axes = plt.gca()
+cur_axes.axes.get_xaxis().set_ticks([])
+cur_axes.axes.get_yaxis().set_ticks([])
+
+plt.xlabel('$x_1$', fontsize = 20)
+plt.ylabel('$x_2$', fontsize = 20)
+plt.show()
+
+
+from cvxopt import matrix, solvers
+# build K
+V = np.concatenate((X0.T, -X1.T), axis = 1)
+K = matrix(V.T.dot(V)) # see definition of V, K near eq (8)
+
+p = matrix(-np.ones((2*N, 1))) # all-one vector 
+# build A, b, G, h 
+G = matrix(-np.eye(2*N)) # for all lambda_n >= 0
+h = matrix(np.zeros((2*N, 1)))
+A = matrix(y) # the equality constrain is actually y^T lambda = 0
+b = matrix(np.zeros((1, 1))) 
+solvers.options['show_progress'] = False
+sol = solvers.qp(K, p, G, h, A, b)
+
+l = np.array(sol['x'])
+print('lambda = ')
+print(l.T)
+
+epsilon = 1e-6 # just a small number, greater than 1e-9
+S = np.where(l > epsilon)[0]
+
+VS = V[:, S]
+XS = X[:, S]
+yS = y[:, S]
+lS = l[S]
+# calculate w and b
+w = VS.dot(lS)
+b = np.mean(yS.T - w.T.dot(XS))
+
+print('w = ', w.T)
+print('b = ', b)
+
+# Saving plot as PDF
+with PdfPages('svm4.pdf') as pdf:
+    # draw
+    # plot points
+    fig, ax = plt.subplots()
+
+    x1 = np.arange(-10, 10, 0.1)
+    y1 = -w[0, 0]/w[1, 0]*x1 - b/w[1, 0]
+    y2 = -w[0, 0]/w[1, 0]*x1 - (b-1)/w[1, 0]
+    y3 = -w[0, 0]/w[1, 0]*x1 - (b+1)/w[1, 0]
+    plt.plot(x1, y1, 'k', linewidth = 3)
+    plt.plot(x1, y2, 'k')
+    plt.plot(x1, y3, 'k')
+
+
+    y4 = 10*x1
+    plt.plot(x1, y1, 'k')
+    plt.fill_between(x1, y1, color='red', alpha=0.1)
+    plt.fill_between(x1, y1, y4, color = 'blue', alpha = .1)
+
+
+    plt.plot(X0[:, 0], X0[:, 1], 'bs', markersize = 8, alpha = .8)
+    plt.plot(X1[:, 0], X1[:, 1], 'ro', markersize = 8, alpha = .8)
+
+    plt.axis('equal')
+    plt.ylim(0, 3)
+    plt.xlim(2, 4)
+
+    # hide ticks 
+    cur_axes = plt.gca()
+    cur_axes.axes.get_xaxis().set_ticks([])
+    cur_axes.axes.get_yaxis().set_ticks([])
+
+    # add circles around support vectors 
+    for m in S:
+        circle = plt.Circle((X[0, m], X[1, m] ), 0.1, color='k', fill = False)
+        ax.add_artist(circle)
+
+
+    plt.xlabel('$x_1$', fontsize = 20)
+    plt.ylabel('$x_2$', fontsize = 20)
+
+    pdf.savefig()  # Save the figure to the PDF
+    plt.show()
